bbc482a8e73b404d44b7ce12ec08f587fdd5dce0
Matt Oskamp <mattoskamp@gmail.com>
2025-01-22 11:55:14 -0500

    postprocessing effects

--- src/components/post-effects.tsx
+++ src/components/post-effects.tsx
@@ -4,6 +4,7 @@ import {
   DepthOfField,
   EffectComposer,
   Noise,
+  Scanline,
   Vignette,
 } from "@react-three/postprocessing";
 import { BlendFunction } from "postprocessing";
@@ -18,14 +19,18 @@ export function PostEffects() {
         bokehScale={2}
         height={480}
       /> */}
+      <Bloom luminanceThreshold={0.1} luminanceSmoothing={1} height={300} />
       <ChromaticAberration
-        offset={new Vector2(0.002, 0)}
+        offset={new Vector2(0.002, 0.0002)}
         radialModulation={false}
         modulationOffset={0.0005}
       />
-      <Bloom luminanceThreshold={0.1} luminanceSmoothing={1} height={300} />
-      <Noise opacity={0.05} />
-      <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      <Noise opacity={0.09} />
+      <Scanline
+        blendFunction={BlendFunction.OVERLAY} // blend mode
+        density={0.9} // scanline density
+      />
+      <Vignette eskil={false} offset={0.01} darkness={0.9} />
     </EffectComposer>
   );
 }
